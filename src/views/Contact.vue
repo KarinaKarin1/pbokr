42f6c255d9c464acf712c7eab83d85957f53546f
KarinaKarin1 <93563149+KarinaKarin1@users.noreply.github.com>
2022-03-07 13:25:33 +0700

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,14 +4,19 @@
       <h1>Contact</h1>
       <peta />
     </div>
+     <div class="col-md-12">
+       <formContact />
+     </div>
   </div>
 </template>
 
 <script>
 import peta from '../components/peta.vue'
+import  FormContact from '../components/FormContact.vue'
   export default {
     components : {
       peta,
+        FormContact,
   }
 }
 </script>
